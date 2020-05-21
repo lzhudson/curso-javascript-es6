cb6e9f09de73e73b088ff900c14b73cc93cc221a
Hudson Holanda <hudsonholanda55@gmail.com>
2020-05-21 20:44:46 -0300

    adicionando aplicação que engloba todos os conceitos

---
+++ aplicacao-com-es6/aplicacao/webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+	entry: ['@babel/polyfill','./src/main.js'],
+	output: {
+		path: __dirname + '/public/',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: __dirname + '/public/'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		],
+	},
+};
